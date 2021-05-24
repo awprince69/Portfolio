043fe2b654c4ee33c77c0f55fe3476d342657fd1
awprince69 <kali.nslookup@gmail.com>
2021-05-24 18:25:59 +0600

    animation

--- src/Component/Home/HeaderMain/HeaderMain.js
+++ src/Component/Home/HeaderMain/HeaderMain.js
@@ -4,34 +4,52 @@ import TypewriterComponent from 'typewriter-effect';
 import photo from '../../../images/prince.jpg'
 import './HeaderMain.css'
 import ScrollDown from './ScrollDown/ScrollDown';
+import Slide from 'react-reveal/Slide';
+import Zoom from 'react-reveal/Zoom';
+import Bounce from 'react-reveal/Bounce';
 
 const HeaderMain = () => {
-    
+
     return (
         <section >
             <div className='container d-flex justify-content-center'>
                 <div className='row'>
                     <div className='col-sm-12 col-lg-4 col-md-6 profile-photo'>
-                        <img src={photo} alt="" />
+                        <Slide left duration={2000}>
+                            <img src={photo} alt="" />
+                        </Slide>
                     </div>
                     <div className='col-sm-12 col-lg-7 col-md-6 mt-3 pt-1'>
-                        <p style={{ fontSize: '20px' }}>Hi,I am</p>
-                        <h1>ABDUL WADUD PRINCE</h1>
-                        <h6 className='type-writer'>
-                            <TypewriterComponent
-                                options={{
-                                    strings: ['Web Developer ', 'Programmer'],
-                                    autoStart: true,
-                                    loop: true,
-                                }}
-                            />
-                        </h6>
-                        <p>I see myself as a committed person. To seek after a difficult vocation in a cutthroat world
-                        where energy, trustworthiness, challenging works, and enthusiasm will uphold me to develop myself. i want to contribute my insight and ability for proficient advancement in the pertinent field. As a  programmer, I love taking challenges and love being a part of the solutions. My passion is to keep myself updated with all the latest technologies and tools.
-                    </p>
-                        <div className='pt-2'>
-                            <a href="https://drive.google.com/file/d/1qMjPY3eQ6xDwLD6hiXX5Jva5p_5VwCEm/view?usp=sharing" target='_blank' rel="noreferrer"><button className='home-button'>Download Resume</button></a>
-                            <Link to='/contact' className='btn btn-outline-info' style={{ borderRadius: '20px' }}>Get In Touch</Link>
+                        <Zoom top duration={2500}>
+                            <p style={{ fontSize: '20px' }}>Hi,I am</p>
+                        </Zoom>
+                        <Slide right duration={2000}>
+                            <h1>ABDUL WADUD PRINCE</h1>
+                            <h6 className='type-writer'>
+                                <TypewriterComponent
+                                    options={{
+                                        strings: ['Web Developer ', 'Programmer'],
+                                        autoStart: true,
+                                        loop: true,
+                                    }}
+                                />
+                            </h6>
+                            <p>I see myself as a committed person. To seek after a difficult vocation in a cutthroat world
+                            where energy, trustworthiness, challenging works, and enthusiasm will uphold me to develop myself. i want to contribute my insight and ability for proficient advancement in the pertinent field. As a  programmer, I love taking challenges and love being a part of the solutions. My passion is to keep myself updated with all the latest technologies and tools.
+                            </p>
+                        </Slide>
+
+                        <div className='pt-2 d-flex'>
+                            <Zoom bottom duration={2500}>
+                                <div>
+                                    <a href="https://drive.google.com/file/d/1qMjPY3eQ6xDwLD6hiXX5Jva5p_5VwCEm/view?usp=sharing" target='_blank' rel="noreferrer"><button className='home-button'>Download Resume</button></a>
+                                </div>
+                            </Zoom>
+                            <Bounce right duration={3000}>
+                                <div className='pt-2'>
+                                    <Link to='/contact' className='btn btn-outline-info' style={{ borderRadius: '20px' }}>Get In Touch</Link>
+                                </div>
+                            </Bounce>
                         </div>
                     </div>
                 </div>
